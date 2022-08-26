27accbfa79d0b5a3f9fa2b278a7934f851ea75f6
Andrew Siems <siemsdrew@gmail.com>
2022-08-26 13:53:41 -0700

    Initial commit

---
+++ main.rb
@@ -0,0 +1,99 @@
+
+arr = [["1"," | ","2"," | ","3"], ["---+---+---"], ["4"," | ","5"," | ","6"],
+["---+---+---"], ["7"," | ","8"," | ","9"]] 
+
+flag = false
+
+module Game
+  WINNING_COMBOS = [["1","2","3"], ["4","5","6"], ["7","8","9"], ["1","4","7"], 
+  ["2","5","8"], ["3","6","9"], ["1","5","9"], ["3","5","7"]]
+
+  def Game.update_grid(grid, choice, marker)
+    grid.each do |row|
+      if row.include?(choice)
+        index = row.index(choice)
+        row[index] = marker
+        break
+      end
+    end
+    grid
+  end
+
+  def Game.print_grid(grid)
+    grid.each do |row|
+      row.each do |char|
+        print char
+      end
+      print "\n"
+    end
+  end
+
+  def Game.check_winner(player_choices)
+    WINNING_COMBOS.each do |row|
+      if (row - player_choices).empty?
+        return true
+        break
+      else
+        return false
+      end
+    end
+  end
+
+end
+
+class Player
+  attr_accessor :name, :marker, :moves
+  include Game 
+  def initialize(name, marker)
+    self.name = name
+    self.marker = marker
+    self.moves = []
+  end
+  def pick_square
+    puts "\n#{self.name}, please pick an available square (1-9): "
+    square = gets.chomp
+    self.moves.push(square)
+  end
+end 
+
+puts "Let's play Tic-Tac-Toe!"
+puts "\nPlease enter the name of Player 1: "
+player_one = gets.chomp
+puts "\nPlease enter the name of Player 2: "
+player_two = gets.chomp
+puts "\nOkay, #{player_one} will be X and #{player_two} will be O!\n"
+
+player_one = Player.new(player_one, "X")
+player_two = Player.new(player_two, "O")
+
+Game.print_grid(arr)
+
+i = 0
+while i < 9
+
+  if i == 8
+    puts "It's a tie!"
+  end
+
+  player_one.pick_square
+  arr = Game.update_grid(arr, player_one.moves[i], player_one.marker)
+  Game.print_grid(arr)
+  if Game.check_winner(player_one.moves) == true
+    puts "GAME OVER! #{player_one.name} is the winner!"
+    break
+  end
+  i += 1
+
+  player_two.pick_square
+  arr = Game.update_grid(arr, player_two.moves[i], player_two.marker)
+  Game.print_grid(arr)
+  if Game.check_winner(player_two.moves) == true
+    puts "GAME OVER! #{player_two.name} is the winner!"
+    break
+  end
+  i += 1
+
+end
+
+
+
